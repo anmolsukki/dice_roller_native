79cffb859f0cb4b9f7ee47492d0edefec59f11a6
anmol <anmol.s@gyftr.com>
2021-09-02 01:28:42 +0530

    Dice Roller

---
+++ src/Screen/DiceRoller.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Pressable } from 'react-native';
+
+const App = () => {
+  const [dice, setDice] = useState(require('../assets/dice1.png'));
+
+  const playBtnTapped = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    switch (randomNumber) {
+      case 1:
+        setDice(require('../assets/dice1.png'));
+        break;
+      case 2:
+        setDice(require('../assets/dice2.png'));
+        break;
+      case 3:
+        setDice(require('../assets/dice3.png'));
+        break;
+      case 4:
+        setDice(require('../assets/dice4.png'));
+        break;
+      case 5:
+        setDice(require('../assets/dice5.png'));
+        break;
+      case 6:
+        setDice(require('../assets/dice6.png'));
+        break;
+      default:
+        setDice(require('../assets/dice6.png'));
+        break;
+    }
+  };
+
+  return (
+    <View style={styles.Container}>
+      <Pressable onPress={() => playBtnTapped()}>
+        <Image style={styles.imgStyle} source={dice} />
+      </Pressable>
+      <TouchableOpacity onPress={() => playBtnTapped()}>
+        <Text style={styles.playBtn}>Play Game</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    backgroundColor: '#222831',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgStyle: {
+    width: 200,
+    height: 200,
+  },
+  playBtn: {
+    fontSize: 20,
+    marginTop: 30,
+    color: '#f2a365',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderColor: '#30475e',
+    borderRadius: 5,
+    borderWidth: 3,
+    fontWeight: 'bold',
+  },
+});
